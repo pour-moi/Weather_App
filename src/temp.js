3fbead0faac4f87b9a9b76d51069194ed1f120cc
pour-moi <pour64moi@gmail.com>
2023-12-07 10:25:55 +0300

    added function to get icon of weather

--- src/temp.js
+++ src/temp.js
@@ -2,6 +2,23 @@ const apiKey = "fe94effad13648a2a69224624230612";
 const location = "Addis Ababa";
 const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
 
+function getWeatherIcon(weatherCondition) {
+  const weatherIcons = {
+    Sunny: "../weather_icons/sunny.svg",
+    Cloudy: "../weather_icons/cloudy.svg",
+    Rainy: "../weather_icons/Raining.svg",
+    Snowy: "../weather_icons/cloud+snow.svg",
+    Windy: "../weather_icons/Windy.svg",
+    Foggy: "../weather_icons/cloudy.svg",
+    Hazy: "../weather_icons/cloud+snow.svg",
+    Thunderstorm: "../weather_icons/Thunder.svg",
+    "Partly cloudy": "../weather_icons/cloud_sunnny.svg",
+    "Mostly Sunny": "../weather_icons/sunny+cloud.svg",
+  };
+
+  return weatherIcons[weatherCondition];
+}
+
 export async function temp() {
   try {
     const response = await fetch(apiUrl);
@@ -9,9 +26,12 @@ export async function temp() {
     console.log(data);
     const currentTemp = document.getElementById("currenttemp");
     const condition = document.querySelector(".condition");
+    const icons = document.querySelector(".cloud");
+    const weatherAtmo = getWeatherIcon(data.current.condition.text);
 
     currentTemp.textContent = `${data.current.temp_c}°`;
     condition.textContent = `${data.current.condition.text}`;
+    icons.src = weatherAtmo;
   } catch (error) {}
 }
 temp();
