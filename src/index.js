790f5e134aafdaf4377507adda58103009bd0319
pour-moi <pour64moi@gmail.com>
2023-12-07 19:32:38 +0300

    removed the imported files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import "./style.css";
 import { format, add } from "date-fns";
 import { temp, apiKey, location, getWeatherIcon } from "./temp.js";
-import sunnyIcon from "../weather_icons/sunny.svg";
-import cloudyIcon from "../weather_icons/cloudy.svg";
-import rainyIcon from "../weather_icons/Raining.svg";
-import snowyIcon from "../weather_icons/cloud+snow.svg";
-import windyIcon from "../weather_icons/Windy.svg";
-import foggyIcon from "../weather_icons/cloudy.svg";
-import hazyIcon from "../weather_icons/cloud+snow.svg";
-import thunderstormIcon from "../weather_icons/Thunder.svg";
-import partlyCloudyIcon from "../weather_icons/cloud_sunnny.svg";
-import mostlySunnyIcon from "../weather_icons/sunny+cloud.svg";
-import patchyRainPossibleIcon from "../weather_icons/rain+sunny.svg";
-import locationicon from "../weather_icons/18545250741556274015.svg";
 
 temp();
 const weekContainer = document.querySelector(".date-container");
