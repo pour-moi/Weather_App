7230dd94bdfffdcea79372ccaee821c820066dba
pour-moi <pour64moi@gmail.com>
2023-12-07 19:44:30 +0300

    doc files

---
+++ docs/9654b0e6c4a2451f3bb3.js
@@ -0,0 +1,33 @@
+import "./style.css";
+import { format, add } from "date-fns";
+import { temp, apiKey, location, getWeatherIcon } from "./temp.js";
+
+temp();
+const weekContainer = document.querySelector(".date-container");
+const nextDaysUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location()}&days=5`;
+
+for (let i = 0; i < 5; i++) {
+  let date = add(new Date(), { days: i });
+  let weekDay = document.createElement("div");
+  weekDay.textContent = format(date, "EEE");
+  weekDay.classList.add("date");
+  weekContainer.appendChild(weekDay);
+}
+
+async function getNextDaysWeather() {
+  const response = await fetch(nextDaysUrl);
+  const datas = await response.json();
+
+  const forecast = datas.forecast.forecastday;
+  const weathers = document.querySelector(".icon-container");
+
+  forecast.forEach((day) => {
+    let weatherDays = document.createElement("img");
+    let iconsForWeekdays = getWeatherIcon(day.day.condition.text);
+    weatherDays.src = iconsForWeekdays;
+    weatherDays.classList.add("weathers");
+    weathers.appendChild(weatherDays);
+  });
+}
+
+getNextDaysWeather();
